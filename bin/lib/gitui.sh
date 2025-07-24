8ee5ddb885094704ed7c3b6eb884a71163b4c2bc
Mark Van de Vyver <mark@taqtiqa.com>
2025-07-24 22:02:57 +1000

    feat: Helix, Yazi & Scooter

---
+++ bin/lib/gitui.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+gitui
+exit_code=$?
+
+if [[ $exit_code -eq 0 ]]; then
+    zellij action toggle-floating-panes
+    zellij action write-chars ":reload-all"
+    zellij action write 13 # send <Enter> key
+else
+	  zellij action toggle-floating-panes
+fi
---
+++ bin/lib/zide-launch.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if zellij ls -n | grep -E '^ZHIDE-Launch .*EXITED' &>/dev/null; then
+    alacritty --title ZHIDE --config-file "${ZIDE_CONFIG_DIR}/alacritty/alacritty.toml" --command /usr/local/bin/zellij delete-session ZHIDE-Launch --force
+fi
+alacritty --title ZHIDE --config-file "${ZIDE_CONFIG_DIR}/alacritty/alacritty.toml" --command /usr/local/bin/zellij delete-session ZHIDE-Launch --force
+#if zellij ls -n | grep -E '^ZHIDE-Launch ' &>/dev/null; then
+#    alacritty --title ZHIDE --config-file "${ZIDE_CONFIG_DIR}/alacritty/alacritty.toml" --command zellij attach ZHIDE-Launch
+#else
+    # We don't have a ZHIDE-Launch session yet
+    alacritty --title ZHIDE --config-file "${ZIDE_CONFIG_DIR}/alacritty/alacritty.toml" --command /usr/local/bin/zellij --session ZHIDE-Launch --new-session-with-layout zide-launch --config-dir "${ZIDE_CONFIG_DIR}/zellij"
+    sleep 2s
+    # Start session-manager plugin
+    #sleep 3s
+    #zellij --session ZHIDE-Launch action launch-or-focus-plugin zellij:session-manager
+    #zellij --session ZHIDE-Launch action move-pane right
+    #zellij --session ZHIDE-Launch action rename-pane "Project Manager"
+#fi
+
+
